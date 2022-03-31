1c23da6428ad3e8c52d63f1d13517eb64488c8f7
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-31 16:25:07 -0400

    icons

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/thomas-marra/" target="_blank"></a> 
+        <a href="https://github.com/tmarra122333" target="_blank"></a> 
+            
+    </div>
+  )
+}
+
+export default HeaderSocials
