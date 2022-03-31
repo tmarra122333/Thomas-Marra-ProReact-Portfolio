a90fe8f9459432a02b185e69d701ecf4e4cdf2fd
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-31 15:30:56 -0400

    Up and Running

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,12 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container"></div>
+      <h5>Hello I'm</h5>
+      <h1>Tommy Marra</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+    </header>
   )
 }
 
