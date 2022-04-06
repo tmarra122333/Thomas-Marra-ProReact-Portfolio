8b7e92b22300b2a56d2d30805d5e4fb82a8364a3
Tmarra122333 <Tmarra122333@gmail.com>
2022-04-06 17:42:43 -0400

    about me done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/CosmoTom.PNG'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return (
@@ -9,21 +12,43 @@ const About = () => {
     <h2> About me</h2>
 
     <div className="container about_container">
-      <div className="about_me">
+      <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About Image" />
           </div>
         </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur
-             adipisicing elit. Laborum adipisci consequatur rem 
-             officia ex laboriosam illum? Facilis earum placeat 
-             ducimus maiores cumque eligendi voluptates aliquid 
-             doloremque, non ea cum repudiandae.
-          </p>
+
+        <div className="about__content">
+         <div className="about__cards">
+
+           <article className='about__card'>
+             <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>General Assembly Software Engineer Graduate</small>
+           </article>
+           
+           <article className='about__card'>
+             <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>2 In the United States</small>
+           </article>
+           
+           <article className='about__card'>
+             <AiOutlineFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5 completed projects</small>
+           </article>
+             </div>
+            
+             <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, aliquid odit perspiciatis facilis illo corrupti hic repellat illum amet ipsa doloribus est soluta. Corporis aliquam, neque totam ex incidunt eius.
+            </p>
 
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-        </div>
+      </div>
+  </div>
+
+      
 
     
     </section>
