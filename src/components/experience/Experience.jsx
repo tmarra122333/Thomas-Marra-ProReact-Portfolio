2956e595b3b27148fd5d968ba71565cee8d287f0
Tmarra122333 <Tmarra122333@gmail.com>
2022-04-07 15:24:49 -0400

    experience started

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './experience.css'
-
+import {BsPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience__content">
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+            </div>
+            
+        </div>
+        <div className="experience__backend">
+            <h3>Backend Development</h3>
+            <div className="experience__content">
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>NODE.JS</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>NoSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>PSQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className='experience__details'>
+                <BsPatchCheckFill/>
+                <h4>Heroku</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+            </div>
+            
+        </div>
+        </div>
+
+    
+      </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,8 +13,8 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className= {activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className= {activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className= {activeNav === '#portfolio' ? 'active' : ''}><IoMdBriefcase/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className= {activeNav === '#experience' ? 'active' : ''}><FaGlobe/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className= {activeNav === '#portfolio' ? 'active' : ''}><IoMdBriefcase/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className= {activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
   )
