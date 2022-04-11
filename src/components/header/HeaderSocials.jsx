eb014e50da9476efc5852ffcf178a41d14619ea3
Tmarra122333 <Tmarra122333@gmail.com>
2022-04-11 14:50:45 -0400

    another

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/thomas-marra/" target="_blank"><BsLinkedin/></a> 
-        <a href="https://github.com/tmarra122333" target="_blank"><FaGithub/></a> 
+        <a href="https://www.linkedin.com/in/thomas-marra/" target="_blank"><BsLinkedin className="boom"/></a> 
+        <a href="https://github.com/tmarra122333" target="_blank"><FaGithub className="boom"/></a> 
             
     </div>
   )
